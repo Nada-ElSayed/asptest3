26ff92d02894b8e58a9afe89521d6a688f60993e
salma elsayed <ssayed.yousef@gmail.com>
2020-09-09 16:38:42 +0200

    maryam files added

---
+++ app3/app3/Hosp_home.aspx.cs
@@ -0,0 +1,290 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Data;
+
+
+
+namespace Reachout1
+{
+    public partial class ViewProd : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            //Response.Write(Session["field1"]);
+            Response.Write(Session["field2"]);
+            Response.Write(Session["field3"]);
+
+        }
+
+        public void viewOrders(object sender, EventArgs e)
+        {
+            string connStr = ConfigurationManager.ConnectionStrings["MyDbConn"].ToString();
+            SqlConnection conn = new SqlConnection(connStr);
+
+            SqlCommand cmd = new SqlCommand("viewHospitalOrders", conn);
+            cmd.CommandType = CommandType.StoredProcedure;
+
+            var usr = Session["field1"];
+            cmd.Parameters.Add(new SqlParameter("@username", usr));
+            conn.Open();
+            SqlDataReader rdr = cmd.ExecuteReader(CommandBehavior.CloseConnection);
+
+            while (rdr.Read())
+            {
+                //Get the value of the attribute name in the Company table
+                int orderno = rdr.GetInt32(rdr.GetOrdinal("order_id"));
+                string status = rdr.GetString(rdr.GetOrdinal("status"));
+                string price = (rdr.GetSqlDecimal(rdr.GetOrdinal("total_price"))).ToString();
+                int prodno = rdr.GetInt32(rdr.GetOrdinal("product_id"));
+
+                string date = (rdr.GetSqlBoolean(rdr.GetOrdinal("date"))).ToString();
+
+
+
+
+                //Create a new label and add it to the HTML form
+
+                Label lbl_orderno = new Label();
+                lbl_orderno.Text = "<div>" + "   Order No.: " + orderno + "</div>";
+                form1.Controls.Add(lbl_orderno);
+
+
+                Label lbl_status = new Label();
+                lbl_status.Text = "<div>" + "   Order Status:  " + status + "</div>";
+                form1.Controls.Add(lbl_status);
+
+
+                Label lbl_price = new Label();
+                lbl_price.Text = "<div>" + "   Total Price: " + price + "</div>";
+                form1.Controls.Add(lbl_price);
+
+
+                Label lbl_prodno = new Label();
+                lbl_prodno.Text = "<div>" + "   Produt no.: " + prodno + "</div>" + "  <br /> <br />";
+                form1.Controls.Add(lbl_prodno);
+
+                Label lbl_date = new Label();
+                lbl_date.Text = "<div>" + "   Order Plcaement Date: " + date + "</div>" + "  <br /> <br />";
+                form1.Controls.Add(lbl_date);
+
+            }
+
+        }
+
+        public void findGloves(object sender, EventArgs e)
+        {
+            string connStr = ConfigurationManager.ConnectionStrings["MyDbConn"].ToString();
+            SqlConnection conn = new SqlConnection(connStr);
+
+            SqlCommand cmd = new SqlCommand("getProducts", conn);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.Add(new SqlParameter("@type", "GLOV"));
+            conn.Open();
+            SqlDataReader rdr = cmd.ExecuteReader(CommandBehavior.CloseConnection);
+
+
+            while (rdr.Read())
+            {
+                //Get the value of the attribute name in the Company table
+                int Prodserialno = rdr.GetInt32(rdr.GetOrdinal("id"));
+                string ProdName = rdr.GetString(rdr.GetOrdinal("name"));
+                //Get the value of the attribute field in the Company table
+                string Prodfinalprice = (rdr.GetSqlDecimal(rdr.GetOrdinal("unit_price"))).ToString();
+                string Prodprice = (rdr.GetSqlDecimal(rdr.GetOrdinal("unit_price"))).ToString();
+
+                string availability = (rdr.GetSqlBoolean(rdr.GetOrdinal("available"))).ToString();
+                if (rdr.GetSqlBoolean(rdr.GetOrdinal("available")))
+                {
+                    availability = "available";
+                }
+                else availability = "not available";
+
+
+                string color = "__";
+                string descrip;
+                if (!rdr.IsDBNull(rdr.GetOrdinal("description")))
+                {
+                    descrip = (rdr.GetString(rdr.GetOrdinal("description")));
+                }
+                else descrip = "__";
+
+
+                string ProdVendorName = rdr.GetString(rdr.GetOrdinal("name"));
+
+                //Create a new label and add it to the HTML form
+
+                Label lbl_ProdName = new Label();
+                lbl_ProdName.Text = "<div>" + "   name: " + ProdName + "</div>";
+                form1.Controls.Add(lbl_ProdName);
+
+
+                Label lbl_avail = new Label();
+                lbl_avail.Text = "<div>" + "    " + availability + "</div>";
+                form1.Controls.Add(lbl_avail);
+
+
+                Label lbl_price = new Label();
+                lbl_price.Text = "<div>" + "   unit price: " + Prodprice + "</div>";
+                form1.Controls.Add(lbl_price);
+
+
+                Label lbl_descrip = new Label();
+                lbl_descrip.Text = "<div>" + "   description: " + descrip + "</div>" + "  <br /> <br />";
+                form1.Controls.Add(lbl_descrip);
+
+
+                // Button AddToCart_btn = new Button();
+                // AddToCart_btn.Text = "   Add to cart: " ; 
+                // form1.Controls.Add(AddToCart_btn);
+
+            }
+
+        }
+        public void findVents(object sender, EventArgs e)
+        {
+            string connStr = ConfigurationManager.ConnectionStrings["MyDbConn"].ToString();
+            SqlConnection conn = new SqlConnection(connStr);
+
+            SqlCommand cmd = new SqlCommand("getProducts", conn);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.Add(new SqlParameter("@type", "VENT"));
+            conn.Open();
+            SqlDataReader rdr = cmd.ExecuteReader(CommandBehavior.CloseConnection);
+
+
+            while (rdr.Read())
+            {
+                //Get the value of the attribute name in the Company table
+                int Prodserialno = rdr.GetInt32(rdr.GetOrdinal("id"));
+                string ProdName = rdr.GetString(rdr.GetOrdinal("name"));
+                //Get the value of the attribute field in the Company table
+                string Prodfinalprice = (rdr.GetSqlDecimal(rdr.GetOrdinal("unit_price"))).ToString();
+                string Prodprice = (rdr.GetSqlDecimal(rdr.GetOrdinal("unit_price"))).ToString();
+
+                string availability = (rdr.GetSqlBoolean(rdr.GetOrdinal("available"))).ToString();
+                if (rdr.GetSqlBoolean(rdr.GetOrdinal("available")))
+                {
+                    availability = "available";
+                }
+                else availability = "not available";
+
+
+                string color = "__";
+                string descrip;
+                if (!rdr.IsDBNull(rdr.GetOrdinal("description")))
+                {
+                    descrip = (rdr.GetString(rdr.GetOrdinal("description")));
+                }
+                else descrip = "__";
+
+
+                string ProdVendorName = rdr.GetString(rdr.GetOrdinal("name"));
+
+                //Create a new label and add it to the HTML form
+
+                Label lbl_ProdName = new Label();
+                lbl_ProdName.Text = "<div>" + "   name: " + ProdName + "</div>";
+                form1.Controls.Add(lbl_ProdName);
+
+
+                Label lbl_avail = new Label();
+                lbl_avail.Text = "<div>" + "    " + availability + "</div>";
+                form1.Controls.Add(lbl_avail);
+
+
+                Label lbl_price = new Label();
+                lbl_price.Text = "<div>" + "   unit price: " + Prodprice + "</div>";
+                form1.Controls.Add(lbl_price);
+
+
+                Label lbl_descrip = new Label();
+                lbl_descrip.Text = "<div>" + "   description: " + descrip + "</div>" + "  <br /> <br />";
+                form1.Controls.Add(lbl_descrip);
+
+
+                // Button AddToCart_btn = new Button();
+                // AddToCart_btn.Text = "   Add to cart: " ; 
+                // form1.Controls.Add(AddToCart_btn);
+
+            }
+
+        }
+
+        public void findMasks(object sender, EventArgs e)
+        {
+            string connStr = ConfigurationManager.ConnectionStrings["MyDbConn"].ToString();
+            SqlConnection conn = new SqlConnection(connStr);
+
+            SqlCommand cmd = new SqlCommand("getProducts", conn);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.Add(new SqlParameter("@type", "MASK"));
+            conn.Open();
+            SqlDataReader rdr = cmd.ExecuteReader(CommandBehavior.CloseConnection);
+
+
+            while (rdr.Read())
+            {
+                //Get the value of the attribute name in the Company table
+                int Prodserialno = rdr.GetInt32(rdr.GetOrdinal("id"));
+                string ProdName = rdr.GetString(rdr.GetOrdinal("name"));
+                //Get the value of the attribute field in the Company table
+                string Prodfinalprice = (rdr.GetSqlDecimal(rdr.GetOrdinal("unit_price"))).ToString();
+                string Prodprice = (rdr.GetSqlDecimal(rdr.GetOrdinal("unit_price"))).ToString();
+
+                string availability = (rdr.GetSqlBoolean(rdr.GetOrdinal("available"))).ToString();
+                if (rdr.GetSqlBoolean(rdr.GetOrdinal("available")))
+                {
+                    availability = "available";
+                }
+                else availability = "not available";
+
+
+                string color = "__";
+                string descrip;
+                if (!rdr.IsDBNull(rdr.GetOrdinal("description")))
+                {
+                    descrip = (rdr.GetString(rdr.GetOrdinal("description")));
+                }
+                else descrip = "__";
+
+
+                string ProdVendorName = rdr.GetString(rdr.GetOrdinal("name"));
+
+                //Create a new label and add it to the HTML form
+
+                Label lbl_ProdName = new Label();
+                lbl_ProdName.Text = "<div>" + "   name: " + ProdName + "</div>";
+                form1.Controls.Add(lbl_ProdName);
+
+
+                Label lbl_avail = new Label();
+                lbl_avail.Text = "<div>" + "    " + availability + "</div>";
+                form1.Controls.Add(lbl_avail);
+
+
+                Label lbl_price = new Label();
+                lbl_price.Text = "<div>" + "   unit price: " + Prodprice + "</div>";
+                form1.Controls.Add(lbl_price);
+
+
+                Label lbl_descrip = new Label();
+                lbl_descrip.Text = "<div>" + "   description: " + descrip + "</div>" + "  <br /> <br />";
+                form1.Controls.Add(lbl_descrip);
+
+
+                // Button AddToCart_btn = new Button();
+                // AddToCart_btn.Text = "   Add to cart: " ; 
+                // form1.Controls.Add(AddToCart_btn);
+
+            }
+
+        }
+
+    }
+}
---
+++ app3/app3/Start.aspx
@@ -0,0 +1,146 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Start.aspx.cs" Inherits="Reachout1.Start" %>
+
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml" lang="en">
+<head runat="server">
+	<title>ReachOut</title>
+
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<!--===============================================================================================-->	
+	<link rel="icon" type="image/png" href="images/icons/favicon.ico"/>
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor/bootstrap/css/bootstrap.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="fonts/Linearicons-Free-v1.0.0/icon-font.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor/animate/animate.css">
+<!--===============================================================================================-->	
+	<link rel="stylesheet" type="text/css" href="vendor/css-hamburgers/hamburgers.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor/animsition/css/animsition.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor/select2/select2.min.css">
+<!--===============================================================================================-->	
+	<link rel="stylesheet" type="text/css" href="vendor/daterangepicker/daterangepicker.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="assets/css/util.css">
+	<link rel="stylesheet" type="text/css" href="assets/css/startStyle.css">
+<!--===============================================================================================-->
+    
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lobster"/>
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Oswald"/>
+
+</head>
+    
+    
+<body>
+	
+	<div class="limiter">
+		<div class="container-login100">
+			<div class="wrap-login100">
+				<div class="loginForm-title" style="background-image: url(assets/img/gloved.jpg);">
+					
+                    <span style="font-weight: 500;
+  letter-spacing: 0.5px; color:white; font-family: Lobster; font-size: 290%; text-align: center; text-shadow:1.5px 1.5px grey; vertical-align : text-bottom;" >
+            ReachOut
+            </span>
+                    <span class="loginForm-title-1og">
+						Log in
+					</span>
+				</div>
+
+				<form class="login100-form validate-form" id="form1" runat="server">
+					<div class="wrap-input100 validate-input m-b-26" data-validate="Username is required">
+						<span class="label-input">Username</span>
+
+						<asp:TextBox style= "height:45px;" class="input100" ID="txt_username" placeholder="Username" runat="server"  OnTextChanged="txt_username_TextChanged"/>
+
+						<span class="focus-input100"></span>
+					</div>
+
+					<div class="wrap-input100 validate-input m-b-18" data-validate = "Password is required">
+						<span class="label-input">Password</span>
+
+						<asp:TextBox style= "height:45px;"  class="input100" placeholder="Password" ID="txt_password" runat="server" TextMode="Password"/>
+						
+						<span class="focus-input100"></span>
+					</div>
+
+					<!--<div class="flex-sb-m w-full p-b-30">
+					
+
+						<div>
+							<a href="#" class="txt1">
+								Forgot Password?
+							</a>
+						</div>
+					</div>-->
+		
+					<div class="container-login100-form-btn">
+						<asp:Button ID="loginForm_btn" runat="server" Text="Login" onclick="login"/>
+			        </div>
+
+					<span>
+					
+						<br />
+						<asp:Label ID="Fail1" runat="server" Text="" Style="font-family:Helvetica, sans-serif;
+								font-size: 13px;
+								">
+
+						</asp:Label>
+					</span>
+                    
+                    
+					<div class="No_Account">
+            
+            
+               
+						<div style= "font-family:Helvetica, sans-serif;
+						font-size: 13px;
+						line-height: 1.4;
+						color: #999999;
+						" > 
+						Don't have an account?
+						</div>
+               
+
+					<div >
+						<asp:Button ID="Button1" runat="server" Text="Register as a Client" onclick="GoRegCust" Width="200px" style="border:None; height:21px; margin:5px" />
+						<br/> 
+
+						<asp:Button ID="Reg1" runat="server" Text="Register as a Vendor" onclick="GoRegVendor" Width="200px" style="border:None; height:21px; margin:5px" />
+					</div>
+
+				</div>
+
+                    
+			</form>
+			</div>
+		</div>
+	</div>
+	
+<!--===============================================================================================-->
+	<script src="assets/vendor/jquery/jquery-3.2.1.min.js"></script>
+<!--===============================================================================================-->
+	<script src="assets/vendor/animsition/js/animsition.min.js"></script>
+<!--===============================================================================================-->
+	<script src="assets/vendor/bootstrap/js/popper.js"></script>
+	<script src="assets/vendor/bootstrap/js/bootstrap.min.js"></script>
+<!--===============================================================================================-->
+	<script src="assets/vendor/select2/select2.min.js"></script>
+<!--===============================================================================================-->
+	<script src="assets/vendor/daterangepicker/moment.min.js"></script>
+	<script src="assets/vendor/daterangepicker/daterangepicker.js"></script>
+<!--===============================================================================================-->
+	<script src="assets/vendor/countdowntime/countdowntime.js"></script>
+<!--===============================================================================================-->
+	<script src="js/main.js"></script>
+
+</body>
+</html>
