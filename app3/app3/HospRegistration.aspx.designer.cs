1bb8a3a50b7cc980154ba1f100ec69aa07b77d98
Rawan Reda <rawan.reda.academia@gmail.com>
2020-09-14 13:40:51 +0200

    HospitalReg updated

--- app3/app3/HospRegistration.aspx.designer.cs
+++ app3/app3/HospRegistration.aspx.designer.cs
@@ -33,6 +33,15 @@ namespace Reachout1
         protected global::System.Web.UI.WebControls.TextBox txtMessages;
 
         /// <summary>
+        /// txtWarning control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label txtWarning;
+
+        /// <summary>
         /// lbl_username control.
         /// </summary>
         /// <remarks>
